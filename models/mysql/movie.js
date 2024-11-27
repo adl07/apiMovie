569da904e6493a2d519b8d289233074a7d21ba27
Adriel <adrielalegre721@gmail.com>
2024-11-27 12:55:40 -0300

    se modifican vercel.json y movie.js

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const DATABASE_CONFIG = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  port: process.env.DB_PORT || 3306,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "moviesdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
