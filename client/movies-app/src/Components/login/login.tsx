3d2ca4e228b1d7e7b6a5047aa4f41572f1b6e85a
Adriel Alegre <adrielalegre721@gmail.com>
2025-01-17 22:03:37 -0300

    add: se agrega modifacion del front en login

--- client/movies-app/src/Components/login/login.tsx
+++ client/movies-app/src/Components/login/login.tsx
@@ -2,17 +2,46 @@ import React, { useState } from "react"
 
 import UserLogin from "../users/user"
 
+interface UserInfo {
+    username: string;
+    password: string;
+    // Añade aquí otros campos que pueda tener tu usuario
+  }
+
 const Login: React.FC=()=>{
 
     const [useUser, setUseUser] = useState<string>('');
 
     const [usePassword, setUsePassword] = useState<string>('');
 
-    
+    const [userData, setUserData] = useState<UserInfo | null>(null);
 
-    const HandleValue=()=>{
-        UserLogin(useUser, usePassword)
-    }
+
+    const getUser = async (username: string) => {
+        try {
+          const response = await fetch(`https://api-movies-app.vercel.app/movies/user?username=${encodeURIComponent(username)}`);
+          if (!response.ok) {
+            throw new Error('Error en la respuesta del servidor');
+          }
+          const data: UserInfo = await response.json();
+          console.log(data)
+          return data;
+        } catch (error) {
+          console.error('Error al obtener usuario:', error);
+          throw error;
+        }
+      };
+    
+      const HandleValue = async () => {
+        try {
+          const user = await getUser(useUser);
+          setUserData(user);
+          await UserLogin(useUser, usePassword);
+        } catch (error) {
+          console.error('Error en HandleValue:', error);
+          // Aquí puedes manejar el error, por ejemplo, mostrando un mensaje al usuario
+        }
+      };
 
     return(
         <div>
