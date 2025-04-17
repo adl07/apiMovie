c300811f9b34c479dadcca8631599c3b719b4440
Adriel Alegre <adrielalegre721@gmail.com>
2025-04-17 01:15:34 -0300

    bugfix: se agrego regex para validar los tipos de datos en paySummaryform

--- client/movies-app/src/Components/paysummary/paySummary.tsx
+++ client/movies-app/src/Components/paysummary/paySummary.tsx
@@ -84,21 +84,23 @@ export const PaySummary: React.FC<payValues>=({plan, price, email, username})=>{
             {/*Agrego form de formik */}
 
             <form onSubmit={formik.handleSubmit} className="card-inputs">
-                        <input 
-                            placeholder="Número de tarjeta"
-                            id="numberCard"
-                            name="numberCard"
-                            type="text"
-                            value={formik.values.numberCard}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            className="input-card-number"
-                            autoComplete="off"
-
-                        />
-                        <p className="text-xs text-red-500 h-5">
-                            {formik.touched.numberCard && formik.errors.numberCard ? formik.errors.numberCard : ''}
-                        </p>
+                        <div className="container-input-card-number">
+                                <input 
+                                    placeholder="Número de tarjeta"
+                                    id="numberCard"
+                                    name="numberCard"
+                                    type="text"
+                                    value={formik.values.numberCard}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    className="input-card-number"
+                                    autoComplete="off"
+                                />
+                                <p className="text-xs text-red-500 h-5">
+                                    {formik.touched.numberCard && formik.errors.numberCard ? formik.errors.numberCard : ''}
+                                </p>
+                        </div>
+                        
 
                         <div className="inputs-fecven">
                             <div className="container-input-fecvenc">
@@ -133,40 +135,43 @@ export const PaySummary: React.FC<payValues>=({plan, price, email, username})=>{
                                 </p>
                             </div>
                         </div>
-                        <input placeholder="Nombre de la tarjeta"
-                            id="userCard"
-                            name="userCard"
-                            type="userCard"
-                            value={formik.values.userCard}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            className="input-name-card"
-                            autoComplete="off"
-                        />
-                        <p className="text-xs text-red-500 h-5">
-                            {formik.touched.userCard && formik.errors.userCard ? formik.errors.userCard : ''}
-                        </p>
+                        <div className="container-input-name-card">
+                            <input placeholder="Nombre de la tarjeta"
+                                id="userCard"
+                                name="userCard"
+                                type="userCard"
+                                value={formik.values.userCard}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className="input-name-card"
+                                autoComplete="off"
+                            />
+                            <p className="text-xs text-red-500 h-5">
+                                {formik.touched.userCard && formik.errors.userCard ? formik.errors.userCard : ''}
+                            </p>
+                        </div>
+                        
                         {/*
                         <button className={isDisable ? 'btn-siguiente-disable' : 'btn-siguiente'} type="submit">
                             Siguiente
                         </button>
                          */}
-                    </form>
 
-            {/*Fin formik form */}
-            <div>
-                <p>${price} al mes (sin impuestos incluidos)</p>
+            <div className="descrip-price">
+                <span>${price} al mes (sin impuestos incluidos)</span>
                 <p>{plan}</p>
             </div>
             <div className="date-register">
                 <p>Recibiras la notificacion al correo <strong>{email}</strong> , es con el que registraste tu membresia.</p>
                 <p>Recorda iniciar sesion con tu user <strong>{username}</strong></p>
             </div>
-            <p className="copyright">Al hacer clic en el botón «Iniciar membresía», aceptas nuestros Términos de uso y nuestra Declaración de privacidad, y declaras que tienes más de 18 años. Asimismo, entiendes que Netflix continuará tu membresía de manera automática y, hasta que la canceles, te facturará el cargo mensual (actualmente de $ {price} al mes + impuestos aplicables) a través de la forma de pago elegida. Puedes cancelar la membresía en cualquier momento para evitar cargos en el futuro. Para cancelarla, ve a Cuenta y haz clic en «Cancelar membresía».</p>
+                <p className="copyright">Al hacer clic en el botón «Iniciar membresía», aceptas nuestros Términos de uso y nuestra Declaración de privacidad, y declaras que tienes más de 18 años. Asimismo, entiendes que Netflix continuará tu membresía de manera automática y, hasta que la canceles, te facturará el cargo mensual (actualmente de $ {price} al mes + impuestos aplicables) a través de la forma de pago elegida. Puedes cancelar la membresía en cualquier momento para evitar cargos en el futuro. Para cancelarla, ve a Cuenta y haz clic en «Cancelar membresía».</p>
 
-            <div>
-                <button className={isDisable ? 'btn-iniciar-memb-disable' : 'btn-iniciar-memb'}>Iniciar membresia</button>
-            </div>
+                <button className={isDisable ? 'btn-iniciar-memb-disable' : 'btn-iniciar-memb'} type="submit">Iniciar membresia</button>
+                    </form>
+
+            {/*Fin formik form */}
+            
         </div>
     )
 }
