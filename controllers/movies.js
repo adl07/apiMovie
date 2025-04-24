b7265dfa9ccf06273a589d197c19dc3057af8b15
Adriel Alegre <adrielalegre721@gmail.com>
2025-04-23 21:23:09 -0300

    fix: jwt rollback log

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,8 +2,7 @@
 //import { MovieModel } from "../models/mysql/movie.js";
 
 import { validateMovie, validatePartialMovie, validateRegistreUser } from "../schemas/movie.js";
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET
+
 
 export class MovieController {
   constructor({ movieModel }) {
@@ -27,8 +26,6 @@ export class MovieController {
       });
     }
   };
-
-
   
   getUser = async (req, res) => {
     try {
@@ -40,7 +37,6 @@ export class MovieController {
           message: "Usuario no encontrado",
         });
       }
-      
       return res.json(users);
     } catch (error) {
       console.error("Error en controller getUser:", error);
