8fc8b7d1068ca9e346241011e101361af7720ebe
Adriel Alegre <adrielalegre721@gmail.com>
2025-04-24 17:02:16 -0300

    fix: mejoras en reglas de cors

--- index.js
+++ index.js
@@ -10,7 +10,24 @@ export function createApp({ movieModel }) {
 
    // Configuración de CORS mejorada
    app.use(cors({
-    origin: '*',
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "http://localhost:5173",
+        "http://movies.com",
+      ];
+    
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+    
+      if (!origin) {
+        return callback(null, true);
+      }
+    
+      return callback(new Error("Not allowed by CORS"));
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
